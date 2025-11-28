68eed5d03751809bb8f422bf19ef4eca14d12744
mieraf <elaman99619@gmail.com>
2025-11-28 12:54:28 +0300

    add student model

--- backend/student/models.py
+++ backend/student/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class Student(AbstractUser):
+    GENDER_CHOICES = [
+        ('male', 'Male'),
+        ('female', 'Female')
+    ]
+
+    gender = models.CharField(choices=GENDER_CHOICES, max_length=10, null=True, blank=True)
+    date_of_birth = models.DateField(null=True, blank=True)
