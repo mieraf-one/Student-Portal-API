c004c369422393c85d511626655aff8e3bd22f74
mieraf <elaman99619@gmail.com>
2025-11-28 12:46:02 +0300

    add api, student and rest_framework to the settings installed apps

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,6 +25,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'api',
+    'student',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
